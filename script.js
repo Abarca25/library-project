4f2906a979366b02bae1645cd94a5145ecf1ff41
Abarca25 <190577751+Abarca25@users.noreply.github.com.>
2025-01-10 18:10:55 -0800

    JS to add books to library

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let myLibrary = [];
+const addBookButton = document.querySelector("#add-book");
+const addBookContainer = document.querySelector(".add-book-background");
+const submitBook = document.querySelector(".submit-book");
+const bookEntryForm = document.querySelector("#book-entry-form");
 
 function Book(title,name,pages,read){
     this.title = title;
@@ -12,3 +16,59 @@ function addBookToLibrary(title,name,pages,read){
     myLibrary.push(newBook);
     
 }
+
+function displayNewBook(book){
+        const booksContainer = document.querySelector(".books-container");
+        const newBook = document.createElement("div");
+        newBook.classList.add("book");
+
+        const title = document.createElement("h4");
+        const name = document.createElement("p");
+        const pages = document.createElement("p");
+        const read = document.createElement("p")
+        const deleteButton = document.createElement("button")
+        deleteButton.setAttribute("id","delete")
+        const markReadButton = document.createElement("button");
+        markReadButton.setAttribute("id","have-read")
+        
+        title.innerText = book.title.value;
+        name.innerText = `by ${book.author.value}`;
+        pages.innerText = `pages ${book.pages.value}`;
+        read.innerText = `read: ${book.read.value}`;
+        deleteButton.innerText = "Delete";
+        markReadButton.innerText = "Mark Read";
+
+        newBook.appendChild(title);
+        newBook.appendChild(name);
+        newBook.appendChild(pages);
+        newBook.appendChild(read);
+        newBook.appendChild(deleteButton);
+        newBook.appendChild(markReadButton);
+
+        booksContainer.appendChild(newBook);
+}
+
+addBookButton.addEventListener("click", () => {
+    addBookContainer.style.display = "flex";
+})
+
+document.querySelector("form").onsubmit = (e) => {
+    e.preventDefault();
+    addBookToLibrary(e.target.title.value,e.target.author.value,e.target.pages.value,e.target.read.value);
+    displayNewBook(e.target);
+    addBookContainer.style.display = "none";
+    bookEntryForm.reset();
+}
+
+addBookContainer.addEventListener("click", (e) => {
+    if(e.currentTarget === e.target){
+        addBookContainer.style.display = "none";
+        bookEntryForm.reset();
+    }
+    else {
+        addBookContainer.style.display ="flex"
+    }
+        
+}
+
+)
